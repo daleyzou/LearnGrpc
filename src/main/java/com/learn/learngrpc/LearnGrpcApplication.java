dcf0bdbc1f6e2ce420c082da38a5296e0b90a1cb
zoudaifa <13011890796Gg!>
2022-05-06 18:32:32 +0800

    Initial commit

---
+++ src/main/java/com/learn/learngrpc/LearnGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.learn.learngrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGrpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/learngrpc/LearnGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.learngrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
